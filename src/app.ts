8eae0ea380335ca33eac704857e37f2294fd72ed
Rulo Vargas <deoraczo@gmail.com>
2020-09-23 19:11:19 -0500

    fix: add route for file statics

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import router from './routes';
-import morgan from 'morgan';
+import morgan from 'morgan'
+import path from 'path';
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use('/disk', express.static(path.resolve('disk')));
 app.use('/api', router)
 
 
